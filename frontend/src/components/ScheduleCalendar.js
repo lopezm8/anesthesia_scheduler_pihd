88852404fa4da8800a22e5ba80876d25b22f1e51
Mario Lopez <86086710+lopezm8@users.noreply.github.com>
2023-07-06 21:16:17 -0700

    modifying code to accurately schedule based on vacation date range

--- frontend/src/components/ScheduleCalendar.js
+++ frontend/src/components/ScheduleCalendar.js
@@ -28,58 +28,6 @@ function countCalls(events) {
   return callCounts;
 }
 
-function balanceCalls(schedules, callCounts, firstCallAssignment) {
-  console.log('Balancing calls with schedules: ', schedules, ' and callCounts: ', callCounts);
-  function balanceCallType(callType) {
-    let anesthesiologists = Object.keys(callCounts);
-    for (let i = 0; i < anesthesiologists.length; i++) {
-      let anesthesiologist = anesthesiologists[i];
-
-      if (firstCallAssignment && firstCallAssignment.anesthesiologist === anesthesiologist) {
-        continue;
-      }
-
-      if (callCounts[anesthesiologist][callType] > 4) {
-        for (let j = 0; j < schedules.length; j++) {
-          if (schedules[j].anesthesiologist === anesthesiologist && schedules[j].call_type === callType) {
-            for (let k = 0; k < anesthesiologists.length; k++) {
-              let replacement = anesthesiologists[k];
-              if (callCounts[replacement][callType] < 4 && schedules.some(s => s.on_call_date === schedules[j].on_call_date && s.call_type === 'third' && s.anesthesiologist === replacement)) {
-                let replacementScheduleIndex = schedules.findIndex(s => s.on_call_date === schedules[j].on_call_date && s.call_type === 'third' && s.anesthesiologist === replacement);
-                schedules[j].anesthesiologist = replacement;
-                schedules[replacementScheduleIndex].anesthesiologist = anesthesiologist;
-
-                callCounts[anesthesiologist][callType]--;
-                callCounts[replacement][callType]++;
-
-                if(callType === 'first') {
-                  let nextDay = new Date(schedules[j].on_call_date);
-                  nextDay.setDate(nextDay.getDate() + 1);
-                  let nextDayStr = nextDay.toISOString().split('T')[0];
-
-                  let nextDaySchedules = schedules.filter(s => s.on_call_date === nextDayStr);
-                  for (let l = 0; l < nextDaySchedules.length; l++) {
-                    if (nextDaySchedules[l].anesthesiologist === replacement) {
-                      nextDaySchedules[l].anesthesiologist = '';
-                    }
-                    if (nextDaySchedules[l].anesthesiologist === '' && anesthesiologist !== firstCallAssignment.anesthesiologist) {
-                      nextDaySchedules[l].anesthesiologist = anesthesiologist;
-                    }
-                  }
-                }
-
-                break;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  balanceCallType('first');
-  balanceCallType('second');
-}
-
 const ScheduleCalendar = ({ events, selectedDate }) => {
   const [currentDate, setCurrentDate] = React.useState(() => {
     const date = new Date(selectedDate);
@@ -118,28 +66,83 @@ const ScheduleCalendar = ({ events, selectedDate }) => {
 
 const mapStateToProps = state => {
   console.log("State in ScheduleCalendar mapStateToProps: ", state);
-  const schedulesByDate = groupBy(state.schedule.schedules, 'on_call_date');
-  const regularEvents = Object.values(schedulesByDate).flatMap((schedulesForOneDay) => {
-    return schedulesForOneDay.map((schedule, index) => {
-      const [year, month, day] = schedule.on_call_date.split("-");
-      const event = {
-        title: schedule.anesthesiologist,
-        callNumber: index + 1,
-        start: new Date(year, month - 1, day),
-        end: new Date(year, month - 1, day),
-        allDay: true,
-      };
 
-      return event;
-    }).sort((a, b) => a.callNumber - b.callNumber);
+  let schedulesByDate = groupBy(state.schedule.schedules, 'on_call_date');
+  let previousFirstCall = null;
+
+  schedulesByDate = Object.keys(schedulesByDate).sort().reduce(
+    (obj, key) => { 
+      obj[key] = schedulesByDate[key]; 
+      return obj;
+    }, 
+    {}
+  );
+
+  const regularEvents = Object.values(schedulesByDate).flatMap((schedulesForOneDay) => {
+    let onCallAnesthesiologists = new Set();
+
+    return schedulesForOneDay.reduce((events, schedule, index) => {
+      const [year, month, day] = schedule.on_call_date.split("-");
+      const date = new Date(year, month - 1, day);
+
+      if (!isOnVacation(date, schedule.anesthesiologist, state.vacations) &&
+          !(index === 0 && previousFirstCall && previousFirstCall === schedule.anesthesiologist && date.getDay() !== 0) &&
+          !isAlreadyOnCall(schedule.anesthesiologist, onCallAnesthesiologists)) {
+        const event = {
+          title: schedule.anesthesiologist,
+          callNumber: index + 1,
+          start: date,
+          end: new Date(year, month - 1, day),
+          allDay: true,
+        };
+        events.push(event);
+        onCallAnesthesiologists.add(schedule.anesthesiologist);
+      }
+
+      if (index === 0) {
+        previousFirstCall = schedule.anesthesiologist;
+      }
+
+      return events;
+    }, []).sort((a, b) => a.callNumber - b.callNumber);
   });
 
   const events = [...regularEvents].sort((a, b) => a.start - b.start);
   const callCounts = countCalls(events);
 
   return { events, vacations: state.vacations, firstCallAssignments: state.schedule.firstCallAssignments };
-
 };
 
+function isOnVacation(date, anesthesiologist, vacations) {
+  const dateStr = formatDate(date);
+  return vacations.some(vacation => {
+    const adjustedStartDate = new Date(vacation.startDate);
+    adjustedStartDate.setDate(adjustedStartDate.getDate() + 1);
+
+    return (
+      vacation.anesthesiologist === anesthesiologist && 
+      formatDate(adjustedStartDate) <= dateStr && 
+      dateStr < formatDate(vacation.endDate)
+    );
+  });
+}
+
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) 
+    month = '0' + month;
+  if (day.length < 2) 
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+function isAlreadyOnCall(anesthesiologist, onCallAnesthesiologists) {
+  return onCallAnesthesiologists.has(anesthesiologist);
+}
 
 export default connect(mapStateToProps)(ScheduleCalendar);
